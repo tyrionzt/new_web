ef8962150895149859bac763c5213d75196762e8
tyrionzt <931675058@qq.com>
2020-12-08 05:08:52 +0000

    haha

--- website/settings.py
+++ website/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'zt',
-        'USER': 'zhangtao',
+        'USER': 'root',
         'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
